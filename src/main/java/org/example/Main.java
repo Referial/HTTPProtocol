aa39370be0063cf677bed4c313e04cfaf6ac3bc5
Nikita Mayorov <MayorovNikita7@yandex.ru>
2023-06-03 23:33:48 +0500

    фиксация 03.06.2023__23:33

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Main {
+
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000) // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000) // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        // создание объекта запроса с произвольными заголовками
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+// отправка запроса
+        CloseableHttpResponse response = httpClient.execute(request);
+// вывод полученных заголовков
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+// чтение тела ответа
+        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+        System.out.println(body);
+    }
+}
